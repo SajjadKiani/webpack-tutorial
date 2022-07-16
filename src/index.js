65e480c860f3a1d7665ad52b47f87bd9ff4a7e8c
Saji <skm.kiani@gmail.com>
2022-07-16 15:34:19 +0430

    add module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import {sayHello} from './speak.js'
 
 document.getElementById('counterBtn').onclick = () => {
     const counter = document.getElementById('counter')
     counter.textContent = parseInt( counter.textContent ) + 1
+}
+
+document.getElementById('sayhello').onclick = ( ) => {
+    sayHello()
 }
